15f6e5c4aba35fc1411e6df8dad1f8a29ec1ef8e
Michael Navarro <navarro.michael93@gmail.com>
2020-06-15 20:30:17 -0500

    Updates

---
+++ precache-manifest.61fd62bb55957ea89a420dcb961bb8d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "262570405eae30ef99d0c6abb7e1b390",
+    "url": "/pokemon-card-game/index.html"
+  },
+  {
+    "revision": "82fcaec387e039b6c7a8",
+    "url": "/pokemon-card-game/static/css/main.a259fa66.chunk.css"
+  },
+  {
+    "revision": "48510583d9d32a8ef2ed",
+    "url": "/pokemon-card-game/static/js/2.d8aa9f1d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokemon-card-game/static/js/2.d8aa9f1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82fcaec387e039b6c7a8",
+    "url": "/pokemon-card-game/static/js/main.428b4897.chunk.js"
+  },
+  {
+    "revision": "3e6de53e72aebaa2d599",
+    "url": "/pokemon-card-game/static/js/runtime-main.f16bf526.js"
+  }
+]);
